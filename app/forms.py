4a0b1a94fd034b6b1cad0478b719b9aa1b68d466
0xbdg <benjamindguntara@protonmail.com>
2026-01-10 09:58:53 +0800

    dsiajisajdoas

--- app/forms.py
+++ app/forms.py
@@ -15,8 +15,8 @@ class RegistsForm(UserCreationForm):
     fields = ["username","email","password1","password2"]
     
 class LoginForm(AuthenticationForm):
-  username = forms.CharField(required=True,widget=TextInput(attrs={'class':'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white','placeholder':'Username'}))
-  password = forms.CharField(required=True,widget=PasswordInput(attrs={'class':'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white','id':'password','placeholder':'Password'}))
+  username = forms.CharField(required=True,widget=TextInput(attrs={'class':'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400','placeholder':'Username'}))
+  password = forms.CharField(required=True,widget=PasswordInput(attrs={'class':'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400','id':'password','placeholder':'Password'}))
   class Meta:
     model = Account
     fields = ["username","password"]
